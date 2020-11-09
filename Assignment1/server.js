abb3ab0de7144732b9702f21d16f42d2642e1ba4
Jack Gilbride <jackgilbride999@gmail.com>
2020-11-09 16:55:30 +0000

    Seems to be working, need to confirm averages

--- Assignment1/server.js
+++ Assignment1/server.js
@@ -25,7 +25,7 @@ function getWeather(req, res){
 
         response.on('end', () => {
             var openWeatherJSON = JSON.parse(todo)
-            console.log(JSON.stringify(openWeatherJSON))
+        //    console.log(JSON.stringify(openWeatherJSON))
 
             /*
                 The JSON should indicate:
@@ -33,33 +33,41 @@ function getWeather(req, res){
                 - Whether the user should pack for cold (-10..+10), warm (+10..+20) or hot (20+) weather
                 - A summary for the next 5 days including: temperature, wind speed and rainfall
             */
-            var summaryForClient = {
-                city: openWeatherJSON.city.name,
-                country: openWeatherJSON.city.country,
-                rain: false,
-                cold: false,
-                warm: false,
-                hot: false,
-                list: []
-            };
+           if(openWeatherJSON.cod == 404){
+               res.json({
+                   code: 404,
+                   message: openWeatherJSON.message
+               })
+           } else {
+                var summaryForClient = {
+                    code: 200,
+                    city: openWeatherJSON.city.name,
+                    country: openWeatherJSON.city.country,
+                    rain: false,
+                    cold: false,
+                    warm: false,
+                    hot: false,
+                    list: []
+                };
 
-            for(var i = 0; i < openWeatherJSON.list.length; i++){
-                var responseItem = openWeatherJSON.list[i];
-                // take the OpenWeatherMap list item, append relevant items to the summary's list
-                summaryForClient.list[i] = {
-                    date_time: responseItem.dt_txt,
-                    temp: responseItem.main.temp,
-                    wind_speed: responseItem.wind.speed,
-                    rain: 0.00
-                }
-                // Check if the list item has rain, if it does we can index into it and update the current list item
-                // Also set rain to true for the overall array, so the client knows an umbrella is needed
+                var responseItem = openWeatherJSON.list[0];
+
+                var prev_item_date = responseItem.dt_txt.split(" ")[0];
+                var dateIndex = 0;
+                var hourIndex = 0;
+                var dayArray = [
+                    {
+                        date_time: responseItem.dt_txt,
+                        temp: responseItem.main.temp,
+                        wind_speed: responseItem.wind.speed,
+                        rain: 0.00
+                    }
+                ];
+                console.log(dayArray)
                 if (responseItem.rain != null){
-                    summaryForClient.list[i].rain = responseItem.rain['3h'];
+                    dayArray[0].rain = responseItem.rain['3h'];
                     summaryForClient.rain = true;
                 }
-                // The temperature will either fall within the cold, warm of hot range.
-                // Ensure that this is indicated to the client, so the user knows to pack for these temperatures.
                 if (responseItem.main.temp < 10){
                     summaryForClient.cold = true;
                 } else if (responseItem.main.temp < 20){
@@ -67,9 +75,63 @@ function getWeather(req, res){
                 } else {
                     summaryForClient.hot = true;
                 }
+                
+                for(var i = 1; i < openWeatherJSON.list.length; i++){
+                    responseItem = openWeatherJSON.list[i];
+                    var current_item_date = responseItem.dt_txt.split(" ")[0];
+                    if(current_item_date == prev_item_date && i!=openWeatherJSON.list.length-1){
+                        console.log(current_item_date + " equals " + prev_item_date)
+                        hourIndex++;
+                    } 
+                    else {
+                        console.log(current_item_date + " doesn't equal " + prev_item_date)
+                        var sum_temp = 0.00;
+                        var sum_wind_speed = 0.00;
+                        var sum_rain = 0.00;
+                        var numItems = dayArray.length;
+
+                        for(d = 0; d<dayArray.length; d++){
+                            sum_temp += dayArray[d].temp;
+                            sum_wind_speed += dayArray[d].wind_speed;
+                            sum_rain += dayArray[d].rain;
+                        }
+                        
+                        summaryForClient.list[dateIndex] = {
+                            date_time: prev_item_date,
+                            temp: (sum_temp/numItems).toFixed(2),
+                            wind_speed: (sum_wind_speed/numItems).toFixed(2),
+                            rain: (sum_rain).toFixed(2)
+                        }
+                        dateIndex++;
+                        hourIndex = 0;
+                        dayArray = [];
+                    }
+                    dayArray[hourIndex] = {
+                        date_time: responseItem.dt_txt,
+                        temp: responseItem.main.temp,
+                        wind_speed: responseItem.wind.speed,
+                        rain: 0.00
+                    }
+                    if (responseItem.rain != null){
+                        dayArray[hourIndex].rain = responseItem.rain['3h'];
+                        summaryForClient.rain = true;
+                    }
+                    if (responseItem.main.temp < 10){
+                        summaryForClient.cold = true;
+                    } else if (responseItem.main.temp < 20){
+                        summaryForClient.warm = true;
+                    } else {
+                        summaryForClient.hot = true;
+                    }
+                    prev_item_date = current_item_date;
+                }
+
+
+                console.log(todo)
+
+                // Finally, put the JSON in the response to the client
+                res.json(summaryForClient)
             }
-            // Finally, put the JSON in the response to the client
-            res.json(summaryForClient)
         })
     }
     )
