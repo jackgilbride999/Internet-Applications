a1bf94c63c3bd059bc3f4e9f79f674dc6a7b7bbe
jackgilbride999 <jackgilbride999@gmail.com>
2020-12-06 12:15:29 +0000

    Happy with the client, want to tidy up database creation in the server before finishing up

--- Assignment2/server.js
+++ Assignment2/server.js
@@ -1,19 +1,17 @@
-const https = require('https');
 const express = require('express')
 const app = express()
 const port = 3000
 const path = require("path");
 var AWS = require('aws-sdk');
 var s3 = new AWS.S3();
+
 AWS.config.update({
     region: 'us-east-1'
 });
-
 var dynamodb = new AWS.DynamoDB();
 
 
-
-// Set up the app to listen on a port, respond to page requests and return to movie requests
+// Set up the app to listen on a port and respond to requests
 let publicPath = path.resolve(__dirname, "public")
 app.use(express.static(publicPath))
 app.listen(port, () => console.log(`Listening on port ${port}`))
@@ -21,7 +19,7 @@ app.get('/movies/', getMovies)
 app.get('/create/', createDatabase)
 app.get('/delete/', deleteDatabase)
 
-function createDatabase(req, res){
+async function createDatabase(req, res){
     let params = {
         TableName : "Movies",
         KeySchema: [       
@@ -48,13 +46,13 @@ function createDatabase(req, res){
             console.log("Loaded " + data.ContentLength + " bytes");
             let bucketData = JSON.parse(data.Body)
             console.log(bucketData)
-            dynamodb.createTable(params, function(err, data) {
+            dynamodb.createTable(params, async function(err, data) {
                 if (err) {
                     console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
                     return res.status(400).json(err)
                 } else {
                     console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
-                    //return res.status(200).send('Successul')
+                    await dynamodb.waitFor("tableExists", {TableName: "Movies"}).promise();
                     let docClient = new AWS.DynamoDB.DocumentClient();
                     bucketData.forEach(function(movie) {
                         var params = {
@@ -68,19 +66,19 @@ function createDatabase(req, res){
                         docClient.put(params, function(err, data) {
                            if (err) {
                                console.error("Unable to add movie", movie.title, ". Error JSON:", JSON.stringify(err, null, 2));
-                               //return res.status(400).json(err)
                            } else {
                                //console.log("PutItem succeeded:", movie.title);
                            }
                         });
                     });
                     console.log("Successfully populated database");
-                    return res.status(200).send('Successfully created database.')
                 }
             });
           }
         }
       );
+      return res.status(200).send('Successfully created database.')
+
 }
 
 function deleteDatabase(req, res){
@@ -99,15 +97,11 @@ function deleteDatabase(req, res){
     });
 }
 
-// The function to response to forecast requests for a city
 function getMovies(req, res){
-    // Parse out the city and call the OpenWeatherMap API
     let title = req.query.title;
     let year = req.query.year;
     console.log("request received, " + title + ", " + year);
     
-    var docClient = new AWS.DynamoDB.DocumentClient();
-
     var params = {
         TableName : "Movies",
         KeyConditionExpression: "#yr = :yyyy and begins_with(title, :t)",
@@ -120,6 +114,8 @@ function getMovies(req, res){
         }
     };
 
+    var docClient = new AWS.DynamoDB.DocumentClient();
+    
     docClient.query(params, function(err, data) {
         if (err) {
             console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
